7958cf6ae1ffd0296557a8da9e174ce2e91f108e
SathwikNarla <sathwiknarla123@gmail.com>
2023-09-12 11:19:52 +0530

    Answers

--- server.py
+++ server.py
@@ -1,32 +1,26 @@
 from flask import Flask, request, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
-@app.route('/calculator/greeting', methods=['GET'])
+@app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello world!', 200
+    return "Hello world!", 200
 
-@app.route('/calculator/add', methods=['POST'])
+@app.route("/calculator/add", methods=['POST'])
 def add():
-    try:
-        data = request.json
-        first = data.get('first', 0)
-        second = data.get('second', 0)
-        result = first + second
-        return jsonify({"result": result}), 200
-    except Exception as e:
-        return str(e), 400
+    data = request.get_json()
+    first = data['first']
+    second = data['second']
+    result = first + second
+    return jsonify({"result": result}), 200
 
-@app.route('/calculator/subtract', methods=['POST'])
+@app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    try:
-        data = request.json
-        first = data.get('first', 0)
-        second = data.get('second', 0)
-        result = first - second
-        return jsonify({"result": result}), 200
-    except Exception as e:
-        return str(e), 400
+    data = request.get_json()
+    first = data['first']
+    second = data['second']
+    result = first - second
+    return jsonify({"result": result}), 200
 
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')
